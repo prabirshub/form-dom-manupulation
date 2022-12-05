7d6955f87f99e29ce7e75daed900b454ff8aa8fb
Prabir Singh <prabirsinghdigital@gmail.com>
2022-12-05 16:11:11 +0530

    final validation

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function () {
   modal.style.display = 'inline'
@@ -11,21 +13,15 @@ modalCloseBtn.addEventListener('click', function () {
   modal.style.display = 'none'
 })
 
+declineBtn.addEventListener('mouseenter', function () {
+  modalChoiceBtns.classList.toggle('modal-btns-reverse')
+})
+
 consentForm.addEventListener('submit', function (e) {
   e.preventDefault()
 
   const consentFormData = new FormData(consentForm)
   const fullName = consentFormData.get('fullName')
-
-  /*   
-Challenge: 
-1. Create a const to store the user's name and
-   use a FormData method to extract the 
-   submitted name from the FormData object.
-2. Insert the user's name into the HTML string
-   that contains the final message we show our
-   users.
-*/
 
   modalText.innerHTML = `
     <div class="modal-inner-loading">
@@ -46,5 +42,6 @@ Challenge:
             <img src="images/pirate.gif">
         </div>
     `
+    modalCloseBtn.disabled = false
   }, 3000)
 })
